679285d88fe07c9ca6d5b5d3fa48cb4f57e9a854
Qubits-ops <72653472+Qubits-ops@users.noreply.github.com>
2021-03-24 13:15:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name = "Chiffrement",
     version = "0.1",
     description = "Programme qui chiffre en plusieurs systeme de chiffrement",
-    icon_resources=[(1, 'crypto.ico')],
     executables = [Executable("main.py")]
 
 )
