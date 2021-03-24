c54af76a689a1ccf820945a16bc399e164d14af1
Qubits-ops <72653472+Qubits-ops@users.noreply.github.com>
2021-03-24 18:32:55 +0100

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name = "Chiffrement",
-    version = "0.1",
-    description = "Programme qui chiffre en plusieurs systeme de chiffrement",
-    executables = [Executable("main.py")]
-
-)
