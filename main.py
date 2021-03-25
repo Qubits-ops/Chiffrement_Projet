d8e102b1ebd13c5123b67783cb56915e0bcf4768
Qubits-ops <72653472+Qubits-ops@users.noreply.github.com>
2021-03-25 17:00:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from chiff import *
 from math2 import *
 from time import *
+import os
 liste1 = [101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541,547,557,563,569,571,577,587,593,599,601,607,613,617,619,631,641,643,647,653,659,661,673,677,683,691,701,709,719,727,733,739,743,751,757,761,769,773,787,797,809,811,821,823,827,829,839,853,857,859,863,877,881,883,887,907,911,919,929,937,941,947,953,967,971,977,983,991,997]
+path = "/Users/Tarkov/Desktop/general"
 while True:
     print("--------Bienvenue dans la matrix--------")
     print("Que souhaitez-vous faire ? Tapez le chiffre correspondant")
@@ -27,11 +29,25 @@ while True:
                     print("Chiffre de décalage confirmé : " + str(decal))
                     break
                 
-                print(CesarChiff(decal,message_a_chiff))
+                print(CesarChiff(message_a_chiff,decal))
                 file = input("souhaitez vous sauvegarder le message chiffre dans un fichier(oui/non): ")
+                nom_file = input("Qu'elle nom lui donner vous: ") 
                 if file == "oui":
-                    with open("test.txt","w") as f:
-                        f.write(CesarChiff(message_a_chiff,decal))
+                    if not os.path.exists("X"):
+                        os.makedirs("X")
+                    if os.path.exists("X"):
+                        path = "/Users/Tarkov/Desktop/general/X"
+                        completeName = os.path.join(path, nom_file)
+                        print("sauvegarder ici " + completeName)
+                        while True:
+                            if os.path.join(path):
+                                if os.path.exists(nom_file):
+                                    print("se fichier existe deja donner un autre nom")
+                                    continue
+                                break
+                        file1 = open(completeName, "w")
+                        file1.write(CesarChiff(message_a_chiff,decal))
+                        file1.close()
                 else:
                     print("comme vous voudez")
                 """chiff_copier.append(message_chiffre)
