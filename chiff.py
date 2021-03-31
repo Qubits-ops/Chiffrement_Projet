944461adffee5ef3e894f16021f210f2165fe1e6
Qubits-ops <72653472+Qubits-ops@users.noreply.github.com>
2021-03-31 13:21:09 +0200

    Update chiff.py

--- chiff.py
+++ chiff.py
@@ -101,4 +101,23 @@ def QubitChiff(message_a_chiff:str)->str:
         
 #print(QubitChiff("salut"))
     
+from random import *
+from copy import *
+
+alphabet = []
+for nombre in range(97,123):
+    alphabet.append(chr(nombre))
+    
+clef = deepcopy(alphabet)
+shuffle(clef)
+
+message = input("Entrez votre message:")
+
+chiffrage = str()
+
+for lettre in message:
+    chiffrage += clef[ord(lettre)-97]
+
+print(clef)
+print(chiffrage)
 
