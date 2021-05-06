5ebdf8b3cc14492e74aa4dd57ab76fa4ed9a09bf
Qubits-ops <72653472+Qubits-ops@users.noreply.github.com>
2021-05-06 21:37:47 +0200

    Create plateforme.py

---
+++ plateforme.py
@@ -0,0 +1,13 @@
+import platform
+
+def plateforme_detect():
+    """
+        detecte sur qu'elle plateform se trouve l'utilisateur
+    """
+    if platform.system() == "linux":
+        print("vous etes sur linux")
+    elif platform.system() == "Windows":
+        print("vous etes sur Windows")
+    else:
+        print("vous etes sur mac")
+plateforme_detect()
